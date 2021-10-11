9572b44c64c233c4b32d4c95667bcf45ed33a39d
michimani <michimani210@gmail.com>
2021-10-11 22:25:41 +0900

    support GET /2/users/:id/following API #15

---
+++ users/types/follows_parameter.go
@@ -0,0 +1,87 @@
+package types
+
+import (
+	"io"
+	"net/url"
+	"strconv"
+	"strings"
+
+	"github.com/michimani/gotwi/internal/util"
+)
+
+type FollowsMaxResult int
+
+type FollowsFollowingGetParams struct {
+	accessToken string
+
+	// Path parameter
+	ID string
+
+	// Query parameters
+	MaxResult       FollowsMaxResult
+	PaginationToken string
+	Expansions      []string
+	TweetFields     []string
+	UserFields      []string
+}
+
+func (m FollowsMaxResult) Valid() bool {
+	return m > 0 && m <= 1000
+}
+
+func (m FollowsMaxResult) String() string {
+	return strconv.Itoa(int(m))
+}
+
+func (p *FollowsFollowingGetParams) SetAccessToken(token string) {
+	p.accessToken = token
+}
+
+func (p *FollowsFollowingGetParams) AccessToken() string {
+	return p.accessToken
+}
+
+func (p *FollowsFollowingGetParams) ResolveEndpoint(endpointBase string) string {
+	if p.ID == "" {
+		return ""
+	}
+
+	encoded := url.QueryEscape(p.ID)
+	endpoint := strings.Replace(endpointBase, ":id", encoded, 1)
+
+	query := url.Values{}
+	return endpoint + resolveFollowingQuery(query, p.MaxResult, p.PaginationToken, p.Expansions, p.TweetFields, p.UserFields)
+}
+
+func (p *FollowsFollowingGetParams) Body() io.Reader {
+	return nil
+}
+
+func resolveFollowingQuery(q url.Values, max FollowsMaxResult, paginationToken string, expansions, tweetFields, userFields []string) string {
+	if max.Valid() {
+		q.Add("max_results", max.String())
+	}
+
+	if paginationToken != "" {
+		q.Add("pagination_token", paginationToken)
+	}
+
+	if expansions != nil {
+		q.Add("expansions", util.QueryValue(expansions))
+	}
+
+	if tweetFields != nil {
+		q.Add("tweet.fields", util.QueryValue(tweetFields))
+	}
+
+	if userFields != nil {
+		q.Add("user.fields", util.QueryValue(userFields))
+	}
+
+	encoded := q.Encode()
+	if encoded == "" {
+		return ""
+	}
+
+	return "?" + encoded
+}
---
+++ users/types/follows_parameter_test.go
@@ -0,0 +1,214 @@
+package types_test
+
+import (
+	"testing"
+
+	"github.com/michimani/gotwi/users/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_FollowsMaxResult_Valid(t *testing.T) {
+	cases := []struct {
+		name   string
+		max    types.FollowsMaxResult
+		expect bool
+	}{
+		{
+			name:   "ok: 1",
+			max:    types.FollowsMaxResult(1),
+			expect: true,
+		},
+		{
+			name:   "ok: 1000",
+			max:    types.FollowsMaxResult(1000),
+			expect: true,
+		},
+		{
+			name:   "ng: 0",
+			max:    types.FollowsMaxResult(0),
+			expect: false,
+		},
+		{
+			name:   "ng: 1001",
+			max:    types.FollowsMaxResult(1001),
+			expect: false,
+		},
+		{
+			name:   "ng: -1",
+			max:    types.FollowsMaxResult(-1),
+			expect: false,
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(tt *testing.T) {
+			b := c.max.Valid()
+			assert.Equal(tt, c.expect, b)
+		})
+	}
+}
+
+func Test_FollowsMaxResult_String(t *testing.T) {
+	cases := []struct {
+		name   string
+		max    types.FollowsMaxResult
+		expect string
+	}{
+		{
+			name:   "ok: 1",
+			max:    types.FollowsMaxResult(1),
+			expect: "1",
+		},
+		{
+			name:   "ok: 0",
+			max:    types.FollowsMaxResult(0),
+			expect: "0",
+		},
+		{
+			name:   "ok: -1",
+			max:    types.FollowsMaxResult(-1),
+			expect: "-1",
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(tt *testing.T) {
+			s := c.max.String()
+			assert.Equal(tt, c.expect, s)
+		})
+	}
+}
+
+func Test_FollowsFollowingGetParams_SetAccessToken(t *testing.T) {
+	cases := []struct {
+		name   string
+		token  string
+		expect string
+	}{
+		{
+			name:   "normal",
+			token:  "test-token",
+			expect: "test-token",
+		},
+		{
+			name:   "normal: empty",
+			token:  "",
+			expect: "",
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(tt *testing.T) {
+			p := &types.FollowsFollowingGetParams{}
+			p.SetAccessToken(c.token)
+			assert.Equal(tt, c.expect, p.AccessToken())
+		})
+	}
+}
+
+func Test_FollowsFollowingGetParams_ResolveEndpoint(t *testing.T) {
+	const endpointRoot = "test/endpoint/"
+	const endpointBase = "test/endpoint/:id"
+	cases := []struct {
+		name   string
+		params *types.FollowsFollowingGetParams
+		expect string
+	}{
+		{
+			name:   "normal: only required parameter",
+			params: &types.FollowsFollowingGetParams{ID: "test-id"},
+			expect: endpointRoot + "test-id",
+		},
+		{
+			name: "normal: with specific max_result",
+			params: &types.FollowsFollowingGetParams{
+				ID:        "test-id",
+				MaxResult: 111,
+			},
+			expect: endpointRoot + "test-id?max_results=111",
+		},
+		{
+			name: "normal: with pagination_token",
+			params: &types.FollowsFollowingGetParams{
+				ID:              "test-id",
+				PaginationToken: "p-token",
+			},
+			expect: endpointRoot + "test-id?pagination_token=p-token",
+		},
+		{
+			name: "normal: with expansions",
+			params: &types.FollowsFollowingGetParams{
+				ID:         "test-id",
+				Expansions: []string{"ex1", "ex2"},
+			},
+			expect: endpointRoot + "test-id?expansions=ex1%2Cex2",
+		},
+		{
+			name: "normal: with tweets.fields",
+			params: &types.FollowsFollowingGetParams{
+				ID:          "test-id",
+				TweetFields: []string{"tf1", "tf2"},
+			},
+			expect: endpointRoot + "test-id?tweet.fields=tf1%2Ctf2",
+		},
+		{
+			name: "normal: with users.fields",
+			params: &types.FollowsFollowingGetParams{
+				ID:         "test-id",
+				UserFields: []string{"uf1", "uf2"},
+			},
+			expect: endpointRoot + "test-id?user.fields=uf1%2Cuf2",
+		},
+		{
+			name: "normal: all query parameters",
+			params: &types.FollowsFollowingGetParams{
+				ID:              "test-id",
+				MaxResult:       111,
+				PaginationToken: "p-token",
+				Expansions:      []string{"ex"},
+				UserFields:      []string{"uf"},
+				TweetFields:     []string{"tf"},
+			},
+			expect: endpointRoot + "test-id?expansions=ex&max_results=111&pagination_token=p-token&tweet.fields=tf&user.fields=uf",
+		},
+		{
+			name: "normal: has no required parameter",
+			params: &types.FollowsFollowingGetParams{
+				Expansions:  []string{"ex"},
+				UserFields:  []string{"uf"},
+				TweetFields: []string{"tf"},
+			},
+			expect: "",
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(tt *testing.T) {
+			ep := c.params.ResolveEndpoint(endpointBase)
+			assert.Equal(tt, c.expect, ep)
+		})
+	}
+}
+
+func Test_FollowsFollowingGetParams_Body(t *testing.T) {
+	cases := []struct {
+		name   string
+		params *types.FollowsFollowingGetParams
+	}{
+		{
+			name:   "empty params",
+			params: &types.FollowsFollowingGetParams{},
+		},
+		{
+			name:   "some params",
+			params: &types.FollowsFollowingGetParams{ID: "id"},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(tt *testing.T) {
+			r := c.params.Body()
+			assert.Nil(tt, r)
+		})
+	}
+}
