50f66bcb54aa756919fe7d33d06cc6c8df925f72
michimani <michimani210@gmail.com>
2021-10-19 08:18:54 +0900

    support GET /2/users/:id/mentions #37

--- tweets/types/tweet_timelines_response.go
+++ tweets/types/tweet_timelines_response.go
@@ -18,3 +18,20 @@ type TweetTimelinesTweetsResponse struct {
 func (r *TweetTimelinesTweetsResponse) HasPartialError() bool {
 	return !(r.Errors == nil || len(r.Errors) == 0)
 }
+
+type TweetTimelinesMentionsResponse struct {
+	Data     []resources.Tweet `json:"data"`
+	Includes struct {
+		Users  []resources.User  `json:"users,omitempty"`
+		Tweets []resources.Tweet `json:"tweets,omitempty"`
+		Places []resources.Place `json:"places,omitempty"`
+		Media  []resources.Media `json:"media,omitempty"`
+		Polls  []resources.Poll  `json:"polls,omitempty"`
+	} `json:"includes,omitempty"`
+	Meta   resources.TweetTimelineMeta `json:"meta"`
+	Errors []resources.PartialError    `json:"errors,omitempty"`
+}
+
+func (r *TweetTimelinesMentionsResponse) HasPartialError() bool {
+	return !(r.Errors == nil || len(r.Errors) == 0)
+}
