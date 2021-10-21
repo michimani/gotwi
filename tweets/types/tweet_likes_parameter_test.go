83a7c91585ae64c357ff0e532cb94ca97ab51b86
michimani <michimani210@gmail.com>
2021-10-21 22:28:25 +0900

    add test case

--- tweets/types/tweet_likes_parameter_test.go
+++ tweets/types/tweet_likes_parameter_test.go
@@ -297,6 +297,36 @@ func Test_TweetLikesLikedTweetsParams_Body(t *testing.T) {
 	}
 }
 
+func Test_TweetLikesPostParams_ResolveEndpoint(t *testing.T) {
+	const endpointRoot = "test/endpoint/"
+	const endpointBase = "test/endpoint/:id"
+	cases := []struct {
+		name   string
+		params *types.TweetLikesPostParams
+		expect string
+	}{
+		{
+			name:   "normal: only required parameter",
+			params: &types.TweetLikesPostParams{ID: "test-id"},
+			expect: endpointRoot + "test-id",
+		},
+		{
+			name: "normal: has no required parameter",
+			params: &types.TweetLikesPostParams{
+				TweetID: "tid",
+			},
+			expect: "",
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(tt *testing.T) {
+			ep := c.params.ResolveEndpoint(endpointBase)
+			assert.Equal(tt, c.expect, ep)
+		})
+	}
+}
+
 func Test_TweetLikesPostParams_Body(t *testing.T) {
 	cases := []struct {
 		name   string
